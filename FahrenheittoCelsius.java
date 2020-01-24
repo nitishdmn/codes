af4deaad7be0e203c36bac224bea2480fa32ea17
Nitish Dhiman <44253500+nitishdmn@users.noreply.github.com>
2020-01-24 19:16:41 +0530

    Update and rename FahrenheittoCelsius.java  to FahrenheittoCelsius.java

--- FahrenheittoCelsius.java
+++ FahrenheittoCelsius.java
@@ -1,3 +1,6 @@
+/*Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), 
+you need to convert all Fahrenheit values from Start to End at the gap of W,
+into their corresponding Celsius values and print the table.*/
 import java.util.*;
 
 class Main
