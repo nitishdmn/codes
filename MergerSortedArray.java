667cb0194cc1b78f8fe65cb1ddeecc2402068c7f
Nitish Dhiman <44253500+nitishdmn@users.noreply.github.com>
2020-01-27 10:47:19 +0530

    Add files via upload

---
+++ MergerSortedArray.java
@@ -0,0 +1,71 @@
+
+import java.util.Scanner;
+
+
+public class MergerSortedArray
+{
+
+  static Scanner s = new Scanner (System.in);
+  public static int[] takeInput ()
+  {
+
+    int size = s.nextInt ();
+    int[] input = new int[size];
+    for (int i = 0; i < size; i++)
+      {
+	input[i] = s.nextInt ();
+      }
+    return input;
+  }
+
+  public static void main (String[]args)
+  {
+
+    int[] arr1 = takeInput ();
+    int[] arr2 = takeInput ();
+    int ans[] = merge (arr1, arr2);
+    for (int i = 0; i < ans.length; i++)
+      {
+	System.out.print (ans[i] + " ");
+      }
+  }
+  public static int[] merge (int arr1[], int arr2[])
+  {
+
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int ans[] = new int[arr1.length + arr2.length];
+    while (i < arr1.length && j < arr2.length)
+      {
+	if (arr1[i] < arr2[j])
+	  {
+	    ans[k] = arr1[i];
+	    k++;
+	    i++;
+	  }
+	else
+	  {
+	    ans[k] = arr2[j];
+	    j++;
+	    k++;
+	  }
+      }
+    while (i < arr1.length)
+      {
+	ans[k] = arr1[i];
+	k++;
+	i++;
+      }
+    while (j < arr2.length)
+      {
+	ans[k] = arr2[j];
+	j++;
+	k++;
+      }
+    return ans;
+  }
+
+
+}
