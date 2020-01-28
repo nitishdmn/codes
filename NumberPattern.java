9278cf873fb0bbaba6d7e8bc71f4259e4b45680c
Nitish Dhiman <44253500+nitishdmn@users.noreply.github.com>
2020-01-28 21:34:36 +0530

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,44 @@
+package qwer;
+import java.util.Scanner;
+/*Print the following pattern
+4 4 4 4 4 4 4 
+4 3 3 3 3 3 4 
+4 3 2 2 2 3 4 
+4 3 2 1 2 3 4 
+4 3 2 2 2 3 4 
+4 3 3 3 3 3 4 
+4 4 4 4 4 4 4 
+ */
+
+public class NumberPattern {
+static Scanner s = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int n = s.nextInt();
+		printPattern(n);
+		
+	}
+public static void printPattern(int n){
+			
+        int i,a,j;
+        for(i = -(n-1); i < n; i++){
+            a = n;
+            for(j = -(n-1); j < n; j++){
+                if(Math.abs(i) < Math.abs(j)){
+                    if(j < 0){
+                        System.out.print(a+" ");
+                        a--;
+                    }
+                    else{
+                        a++;
+                        System.out.print(a+" ");
+                    }
+                }
+                else{
+                    System.out.print(a+" ");
+                }
+            }
+            System.out.println();
+        }
+	}
+}
